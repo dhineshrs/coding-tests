e258923338affbe5234fb9951fd4662fec70967f
Dhinesh <dhineshkumar.rajendran@ust.com>
2024-05-22 12:56:52 +0530

    project setup

---
+++ src/main/java/com/dlabs/codingtests/CodingTestsApplication.java
@@ -0,0 +1,13 @@
+package com.dlabs.codingtests;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodingTestsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodingTestsApplication.class, args);
+	}
+
+}
